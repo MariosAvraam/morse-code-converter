a9b7ec32bd5a8d52ab76ddd63b59df125725809f
Marios <marios_2510@hotmail.com>
2023-08-09 18:32:31 +0300

    Add reverse dictionary (morse to text). Add function to convert from morse to text. Add user menu with options.

--- main.py
+++ main.py
@@ -16,9 +16,12 @@ MORSE_CODE_DICT = {
     '$': '...-..-', '@': '.--.-.', ' ': '/',
 }
 
+REVERSE_MORSE_CODE_DICT = {value: key for key,
+                           value in MORSE_CODE_DICT.items()}
 
-def string_to_morse(input_string):
-    """Converts a string to Morse Code."""
+
+def convert_to_morse(input_string):
+    """Converts text to Morse Code."""
     morse_code = ''
     for char in input_string:
         if char in MORSE_CODE_DICT:
@@ -29,8 +32,38 @@ def string_to_morse(input_string):
     return morse_code
 
 
-# Get User Input
-user_input = input("Enter a string to convert to Morse Code: ").upper()
+def convert_from_morse(morse_code):
+    """Converts Morse Code to text."""
+    words = morse_code.split('  ')  # Assumes two spaces between words
+    decoded_message = ''
 
-# Conversion and Display
-print(string_to_morse(user_input))
+    for word in words:
+        letters = word.split()
+        for symbol in letters:
+            if symbol in REVERSE_MORSE_CODE_DICT:
+                decoded_message += REVERSE_MORSE_CODE_DICT[symbol]
+            else:
+                decoded_message += '?'
+        decoded_message += ' '
+    return decoded_message.strip()
+
+
+while True:
+    print("\nChoose an option:")
+    print("1. Convert text to Morse code")
+    print("2. Convert Morse code to text")
+    print("3. Exit")
+
+    choice = input("> ").strip()
+
+    if choice == "1":
+        user_input = input("Enter a string to convert to Morse Code: ").upper()
+        print(convert_to_morse(user_input))
+    elif choice == "2":
+        user_input = input("Enter Morse Code to convert to text: ")
+        print(convert_from_morse(user_input))
+    elif choice == "3":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please try again.")
